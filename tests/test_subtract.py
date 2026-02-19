6f251dab400c60de1bd1f84afb2bc71458aed88c
lakshmi1212 <36476419+lakshmi1212@users.noreply.github.com>
2026-02-20 00:08:48 +0530

    Add or update file: test_subtract.py

--- tests/test_subtract.py
+++ tests/test_subtract.py
@@ -1,21 +1,13 @@
 import pytest
 from src.math_operations import subtract
 
-@pytest.mark.parametrize("a, b, expected", [
+@pytest.mark.parametrize("a,b,expected", [
     (3, 2, 1),
-    (1, 1, 0),
     (0, 0, 0),
+    (-1, -1, 0),
+    (-5, 5, -10),
     (2.5, 1.5, 1.0),
-    (-3, -6, 3),
-    (2000000, 1000000, 1000000),
-    (1, -2, 3),
-    (-5, 5, -10)
+    (2000000, 1000000, 1000000)
 ])
 def test_subtract(a, b, expected):
     assert subtract(a, b) == expected
-
-def test_subtract_type_error():
-    with pytest.raises(TypeError):
-        subtract('a', 1)
-    with pytest.raises(TypeError):
-        subtract(1, None)
